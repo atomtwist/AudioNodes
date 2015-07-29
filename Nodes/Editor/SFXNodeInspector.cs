62c8739b1346c103f4613e1fd264780956e174f9
Milan Grajetzki <atomtwist@gmail.com>
2015-07-29 19:22:44 +0200

    added SFXNodeInspector :-)

---
+++ Nodes/Editor/SFXNodeInspector.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SFXNode))]
+[CanEditMultipleObjects]
+public class SFXNodeInspector : Editor {
+
+	SFXNode sfxNode;
+
+	void OnEnable()
+	{
+		sfxNode = target as SFXNode;
+	}
+
+	void AuditionButtons()
+	{
+		EditorGUILayout.BeginVertical("Box");
+		EditorGUILayout.Space();
+		if (GUILayout.Button("Audition Parent"))
+		{
+			var parentNode = sfxNode.transform.parent.GetComponent<AudioNode>();
+			if (parentNode != null)
+				parentNode.Audition();
+			else
+				sfxNode.Audition();
+		}
+		if (GUILayout.Button("Audition"))
+		{
+			sfxNode.Audition();
+		}
+		if (GUILayout.Button("StopAudition"))
+		{
+			sfxNode.StopAudition ();
+		}
+		EditorGUILayout.Space();
+		EditorGUILayout.EndVertical();
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		DrawDefaultInspector();
+		AuditionButtons();
+	}
+
+}
